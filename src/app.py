692a3f888b895e2471ad2669084f35eecb7226c8
Miller Mambuscay <millerrestivenmambuscayvargas@gmail.com>
2023-06-05 11:50:39 -0500

    Avances

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 from config import config
 
@@ -54,6 +54,65 @@ def leer_usuario(id):
           return jsonify({'mensaje': "Error", 'confirm': False})
      
      
+@app.route('/', methods=['POST'])
+def agregar():
+    # if(validateEmail(request.json['email']) and validatePass(request.json['pass'])):
+    try:
+        usuario = leer_usuario_porID(request.json['id'])
+        if usuario != None:
+            return jsonify({'mensaje' : 'Error ya existe el ID','confirm': False})
+        else:
+            cursor= conexion.connection.cursor()
+
+ 
+            # sql = "INSERT INTO country,city (pais, capital_nombre, capital_id) VALUES ('{0}', '{1}', {2})".format(request.json['pais'], request.json['capital_nombre'], request.json['capital_id'])
+
+            #sql = "INSERT INTO c.Name AS Pais, ci.ID AS ID_Ciudad, ci.Name AS Capital FROM country c JOIN city ci ON c.capital = ci.ID WHERE id = '{0}'".format(id)
+
+            cursor.execute(sql)
+            conexion.connection.commit()
+            return jsonify({'mensaje':'usuario registrado','confirm': True})
+    except Exception as ex:
+        return jsonify({'mensaje': 'Error usuario no registrado','confirm':False})   
+    # else:
+    #     return jsonify({'mensaje': 'Valores no validos', 'confirm':False})
+
+
+
+
+def eliminar_usuario_porID(id):
+    try:
+        cursor = conexion.connection.cursor()
+        # sql = "DELETE FROM city WHERE id = '{0}'".format(id)
+        # sql = "DELETE c.Name AS Pais, ci.ID AS ID_Ciudad, ci.Name AS Capital FROM country c JOIN city ci ON c.capital = ci.ID WHERE id  = '{0}'".format(id)
+
+        # sql = "DELETE Pais, city FROM Pais JOIN city ON Pais.ID_Capital = city.ID = '{0}'".format(id)
+        sql = "DELETE country, city FROM country JOIN city ON country.capital = city.Name WHERE city.ID = '{0}'".format(id)
+    
+    
+        cursor.execute(sql)
+        conexion.connection.commit()
+        return True
+    except Exception as ex:
+        raise ex
+
+@app.route('/<id>', methods=['DELETE'])
+def eliminar_usuario(id):
+    try:
+        usuario = leer_usuario_porID(request.json['id'])
+        if eliminar_usuario_porID(id):
+
+            if usuario == None:
+                return jsonify({'mensaje': 'No se encontró el usuario', 'confirm': False})
+            else:
+                return jsonify({'mensaje': 'Usuario eliminado correctamente', 'confirm': True})
+            
+    except Exception as ex:
+        return jsonify({'mensaje': 'Error al eliminar el usuario', 'confirm': False})
+
+
+
+
 
 def pagina_no_encontrada(error):
     return "<h1> Pagina no encontrada</h1>",484
